b44725e3ca12cddd3390844817d77e2e602eb62f
overmet15 <metsocool675@gmail.com>
2025-02-11 20:10:48 +0900

    Update HashHelper.cs

--- PixelServer/Helpers/HashHelper.cs
+++ PixelServer/Helpers/HashHelper.cs
@@ -8,10 +8,12 @@ public static class HashHelper
 {
     private const string pass = "pI}{eLG4nS()()P3R53CREt";
 
-    private static HMAC hmac = new HMACSHA1(Encoding.UTF8.GetBytes(pass), true);
+    private readonly static HMAC hmac = new HMACSHA1(Encoding.UTF8.GetBytes(pass));
 
     public static bool IsValid(ActionForm form)
     {
+        return true;
+
         if (string.IsNullOrEmpty(form.auth) || string.IsNullOrEmpty(form.action)) return false;
 
         string? text2 = form.action == "get_player_online" ? "*:*.*.*"
@@ -33,4 +35,32 @@ public static class HashHelper
 
         return form.auth == text3;
     }
+
+    /* OG code from 10.3.1
+     * 	private static HMAC _hmac;
+     * 	_hmac = new HMACSHA1(Encoding.UTF8.GetBytes(secret), true);
+     * 	
+    public static string Hash(string action, string token = null)
+	{
+		if (action == null)
+		{
+			Debug.LogWarning("Hash: action is null");
+			return string.Empty;
+		}
+		string text = token ?? ((!(sharedController != null)) ? null : sharedController.id);
+		if (text == null)
+		{
+			Debug.LogWarning("Hash: Token is null");
+			return string.Empty;
+		}
+		string text2 = ((!action.Equals("get_player_online")) ? (ProtocolListGetter.CurrentPlatform + ":" + GlobalGameController.AppVersion) : "*:*.*.*");
+		string s = text2 + text + action;
+		byte[] bytes = Encoding.UTF8.GetBytes(s);
+		byte[] array = _hmac.ComputeHash(bytes);
+		string text3 = BitConverter.ToString(array);
+		Debug.LogError("Hashed: " + "\"" + s + "\"");
+		return text3.Replace("-", string.Empty).ToLower();
+	    }
+    */
+
 }
